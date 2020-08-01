9ed3ec469153b20ae281b7d70609cbf424d8d333
Savannah Evans <43726105+sevans5@users.noreply.github.com>
2020-08-01 17:13:58 -0400

    Initial commit

---
+++ build/precache-manifest.0ea7b54b6d9a7203c213947b2a86b575.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f346960a9a251b04a923823c04accca1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f1e47f45b539dd71c1e",
+    "url": "/static/css/2.30dbd4a1.chunk.css"
+  },
+  {
+    "revision": "5fbe2fc0f28fcbdd29e2",
+    "url": "/static/css/main.199d1c2c.chunk.css"
+  },
+  {
+    "revision": "8f1e47f45b539dd71c1e",
+    "url": "/static/js/2.207a80ab.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.207a80ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fbe2fc0f28fcbdd29e2",
+    "url": "/static/js/main.3940a426.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const dotenv = require('dotenv');
+const path = require('path');
+var cors = require('cors')
+
+
+const app = express()
+const port = 5000;
+dotenv.config()
+app.use(cors())
+
+
+
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+app.get('/weather/:city', async (req, res) => {
+    console.log(req.params.city)
+    await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=${process.env.weatherAPIKey}`, {
+    }).then(response => {
+        console.log(response.data)
+        
+        res.send({data: response.data})
+    }).catch(err => {
+        if(err.response.data.cod == '404'){
+            res.send( {data: "404"} )
+        }
+    })
+})
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`))
+
+
+
+
+
+
+
+
+
+
